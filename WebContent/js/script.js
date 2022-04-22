22839fe0db7a820f790a2552e6be4d8d221a93c9
H-shimanaka <h.shimanaka@a-force.co.jp>
2022-04-22 09:51:18 +0900

    html.css.jsファイルの修正

--- WebContent/js/script.js
+++ WebContent/js/script.js
@@ -1,7 +1,7 @@
-'use strict';
 
 $('#page-link a[href*="#"]').click(function(){
-	let elmHash = $(this).attr('href');
-	let pos = $(elmHash).offset().top-150;
-	$(body,html).animate({scrollTop:pos},2000);
+	var elmHash = $(this).attr('href');
+	var pos = $(elmHash).offset().top-150;
+	$('body,html').animate({scrollTop:pos},500);
+	return false;
 });
